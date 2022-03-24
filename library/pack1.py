d86fa87262fc76d63c00c84891a7cbc3767960da
Jan <jan.zaluska@gmail.com>
2022-03-24 16:10:03 +0100

    iteracja 1`

---
+++ library/pack1.py
@@ -0,0 +1,17 @@
+
+
+def dodaj_liczby(licz1, licz2):
+    return licz1 + licz2
+
+def odejmij_liczby(licz1, licz2):
+    return licz1 - licz2
+
+def pomnóż_liczby(licz1, licz2):
+    return licz1 * licz2
+
+def podziel_liczby(licz1, licz2):
+    return licz1 / licz2
+
+def potęguj_liczby(licz1, licz2):
+    return licz1 ** licz2
+print(pomnóż_liczby(1111111111111111,111111111111))
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="kalkulator",
+    version='0.1',
+    description='Prosty kalkulator',
+    url='',
+    author='Jan Załuska',
+    author_email='jzaluska@st.swps.edu.pl',
+    license='unlicense',
+    packages=['library'],
+    zip_safe=False
+)
